6543327aac6f5c663649441563e309f468b1c875
Abu Azis <muhammadazis801@gmail.com>
2020-09-25 17:28:19 +0700

    Updates

---
+++ precache-manifest.0eea2d4f1077b420f706960b8f0f6283.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11afd014f0c4189217421751f2d6e353",
+    "url": "/personal/index.html"
+  },
+  {
+    "revision": "5f2cb2e0efae4f72b1a9",
+    "url": "/personal/static/css/main.ba83890d.chunk.css"
+  },
+  {
+    "revision": "d16a8e3a190bbc4cd1ea",
+    "url": "/personal/static/js/2.c2b89aed.chunk.js"
+  },
+  {
+    "revision": "d3031346469a8a2ef527f1ad705ea3b7",
+    "url": "/personal/static/js/2.c2b89aed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f2cb2e0efae4f72b1a9",
+    "url": "/personal/static/js/main.c936f543.chunk.js"
+  },
+  {
+    "revision": "999d39831430085a098a",
+    "url": "/personal/static/js/runtime-main.56d4c70d.js"
+  },
+  {
+    "revision": "db9b21a1c41f3dcd9731e1e7acfdbb57",
+    "url": "/personal/static/media/google-play-badge.db9b21a1.png"
+  },
+  {
+    "revision": "372920e04e1e22ed1bd834ceab24b310",
+    "url": "/personal/static/media/profile.372920e0.png"
+  }
+]);
