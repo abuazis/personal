9eabe77a97e6bce36be997ffa884f8734dc6ae74
Abu Azis <muhammadazis801@gmail.com>
2020-09-27 20:46:06 +0700

    change padding swicth button

--- src/parts/SwitchTheme.js
+++ src/parts/SwitchTheme.js
@@ -3,7 +3,7 @@ import React from "react";
 class SwitchTheme extends React.Component {
   render() {
     return (
-      <div class="toggle toggle--daynight py-1 px-lg-4">
+      <div class="toggle toggle--daynight py-1 pl-lg-4">
         <input type="checkbox" id="toggle--daynight" class="toggle--checkbox" />
         <label class="toggle--btn" for="toggle--daynight"></label>
       </div>
