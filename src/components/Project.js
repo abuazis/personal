ea378c5cb60125a9ec1afb1e91791ba223bc5c3d
Abu Azis <muhammadazis801@gmail.com>
2020-12-17 09:37:17 +0700

    add some feature

--- src/components/Project.js
+++ src/components/Project.js
@@ -11,28 +11,34 @@ export default function Project(props) {
   return (
     <section id="project" className="container pb-5 mb-5">
       <div className="row align-items-center mt-3">
-        <div className="col-12 mx-sm-3 px-4">
+        <div className="col-12 mx-sm-3 px-4 mx-md-0 px-md-0 mx-xl-3 px-xl-4">
           <h3 className="text-white font-weight-bolder mb-5">Projects</h3>
           {props.data.map((item) => {
             return (
               <Fade>
                 <div className="row mb-5 pb-3">
-                  <div className="col-12 col-lg-6">
-                    <img src={item.image} className="rounded w-100" />
+                  <div className="col-12 col-xl-6">
+                    <a href="#">
+                      <div className="w-100 position-relative">
+                        <div className="w-100 eye-hover rounded position-relative">
+                          <img src={item.image} className="rounded w-100" />
+                        </div>
+                      </div>
+                    </a>
                   </div>
-                  <div className="col-12 col-lg-6 pt-lg-0 mt-lg-0 mt-4 pt-3">
-                    <h3 className="text-primary font-weight-bolder ml-lg-4">
+                  <div className="col-12 col-xl-6 pt-xl-0 mt-xl-0 mt-4 pt-3">
+                    <h3 className="text-primary font-weight-bolder ml-xl-4">
                       {item.name}
                     </h3>
-                    <h5 className="text-white ml-lg-4 desc-project">
+                    <h5 className="text-white ml-xl-4 desc-project">
                       {item.description}{" "}
                     </h5>
-                    <div className="ml-lg-4">
+                    <div className="ml-xl-4">
                       {item.tech.map((item) => {
                         return <Badge name={item.name} />;
                       })}
                     </div>
-                    <div className="ml-lg-3">
+                    <div className="ml-xl-3 ml-0">
                       {item.play_store_link != null ? (
                         <a href={item.play_store_link} target="_blank">
                           <img src={GooglePlay} className="mr-2" width="150" />
@@ -42,7 +48,7 @@ export default function Project(props) {
                       )}
                       <a
                         href={item.github_link}
-                        className="btn btn-github text-decoration-none ml-lg-2"
+                        className="btn btn-github text-decoration-none ml-xl-2"
                         target="_blank"
                       >
                         Source Code{" "}
